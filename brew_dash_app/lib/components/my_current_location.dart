c63293102bc0a5e6908ba03224c2376abb2357c7
Sadekin Hasnath <shasnath2@student.gsu.edu>
2024-04-27 18:02:49 -0400

    Fixed user input location not saving

--- brew_dash_app/lib/components/my_current_location.dart
+++ brew_dash_app/lib/components/my_current_location.dart
@@ -6,40 +6,41 @@ class MyCurrentLocation extends StatelessWidget {
   MyCurrentLocation({super.key});
 
 
-  final textController = TextEditingController();
-  
+   final textController = TextEditingController();
+
   void openLocationSearchBox(BuildContext context) {
-  showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      title: const Text("Your location"),
-      content: const TextField(
-        decoration: InputDecoration(hintText: "Enter address..."),
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        title: const Text("Your location"),
+        content: TextField(
+          controller: textController, // Connect the TextEditingController to the TextField
+          decoration: const InputDecoration(hintText: "Enter address..."),
+        ), 
+        actions: [
+          // cancel button
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+              textController.clear(); // This will clear the text field upon closing the dialog
+            },
+            child: const Text('Cancel'),
+          ), 
+          // save button
+          MaterialButton(
+            onPressed: () {
+              String newAddress = textController.text; // Retrieve the text from the controller
+              context.read<Restaurant>().updateDELIVERYADDRESS(newAddress);
+              Navigator.pop(context);
+              textController.clear(); // This will clear the text field upon saving the address
+            },
+            child: const Text('Save'),
+          ), 
+        ],
       ), 
-      actions: [
-        // cancel button
-        MaterialButton(
-          onPressed: () {
-            Navigator.pop(context);
-            textController.clear();
-          },
-          child: const Text('Cancel'),
-        ), 
-        // save button
-        MaterialButton(
-          onPressed: () {
-            String newAddress = textController.text;
-            context.read<Restaurant>().updateDELIVERYADDRESS(newAddress);
-            Navigator.pop(context);
-            textController.clear();
-          },
-          child: const Text('Save'),
-        ), 
-      ],
-    ), 
-  );
-}
+    );
+  }
 
  @override
   Widget build(BuildContext context) {
